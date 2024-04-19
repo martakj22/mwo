745b5fd2bdf4e3d22e273ccd8873495e533db581
marta j <dsw55591@student.dsw.edu.pl>
2024-04-19 19:51:43 +0200

    Managing rockets now

--- SomeProgram.java
+++ SomeProgram.java
@@ -4,4 +4,9 @@ public class SomeProgram {
         System.out.println("we are learning to use git.");
         System.out.println("we will be git masters, soon.");
     }
+
+public void manageRockets(){
+    
+}
+
 }
