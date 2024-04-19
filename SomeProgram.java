c7223cb539a9dcea7167bc699c9c89e0ebd34052
marta j <dsw55591@student.dsw.edu.pl>
2024-04-19 19:18:00 +0200

    Add second implementation of the program

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,5 +2,6 @@ public class SomeProgram {
 
     public static void main(String[] args) {
         System.out.println("we are learning to use git.");
+        System.out.println("we will be git masters, soon.");
     }
 }
